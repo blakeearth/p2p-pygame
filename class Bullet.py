93f01b1c9bbb0901433fea129afb9ba44261875a
CooperUrbec <49803257+CooperUrbec@users.noreply.github.com>
2019-05-03 06:23:03 -0400

    finished bullet class

--- class Bullet.py
+++ class Bullet.py
@@ -1,14 +1,27 @@
 class Bullet:
-    gravity = 9.8
+    gravity = -9.8
 
     def __init__(self, x, y, xVol, yVol):
         self.x = x
         self.y = y
+        self.yStart = self.y
         self.xVolocity = xVol
         self.yVolocity = yVol
+        self.yInitialVol = self.yVolocity
+        self.time = 0
 
     def update(self):
-        pass
+        global gravity
+
+        self.time += 1
+
+        newX = self.x + self.xVolocity
+        newVol = self.yInitialVol + (gravity * self.time)
+
+        newY = self.yStart + ((1/2) * (self.yInitialVol + newVol) * self.time)
+
+        self.x = newX
+        self.y = newY
 
     def getx(self):
         return self.x
